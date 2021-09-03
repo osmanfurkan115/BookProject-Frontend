ee56895d147d49623b80aeca7db2a19653ee0188
Osman Furkan <osmanfurkan115@gmail.com>
2021-09-03 23:10:50 +0300

    Added rate feature

--- src/pages/books/Books.tsx
+++ src/pages/books/Books.tsx
@@ -1,36 +1,68 @@
 import { ReactElement, useEffect, useState } from "react";
-import { Segment } from "semantic-ui-react";
+import { Button, Segment } from "semantic-ui-react";
 import Navi from "../../layouts/Navi";
 import BookService from "../../services/bookService";
 import "./Books.css";
 
 export interface Book {
-  id: number,
-  name: string,
-  author: Author
+  id: number;
+  name: string;
+  description: string;
+  author: Author;
+  rateAmount: number;
+  pageAmount: number;
+  publishingYear: number;
+  createdTime: Date;
 }
 
 export interface Author {
-  id: number,
-  name: string
+  id: number;
+  name: string;
 }
 
 export default function Books(): ReactElement {
   const [books, setBooks] = useState<Book[]>([]);
 
+  const [isRated, setRated] = useState(false);
+  let bookService = new BookService();
+
   useEffect(() => {
     let bookService = new BookService();
     bookService.getBooks().then((book) => setBooks(book));
+
   }, []);
+
+  function handleRate(book: Book): void {
+    const newBook = {...book, rateAmount: book.rateAmount+1};
+    bookService.updateBook(newBook);
+    setRated(true);
+    books[books.indexOf(book)] = newBook
+    setBooks(books);
+  }
 
   return (
     <div>
       <Navi />
       <div className="book">
         <Segment.Group>
-          {books.map(book => (
+          {books.map((book) => (
             <Segment key={book.id} secondary raised>
-              <h1>{book.name} - {book.author.name}</h1>
+              <h1>
+                {book.name} - {book.author.name}
+              </h1>
+              <Button
+                color="orange"
+                content="Rate"
+                icon="star"
+                disabled={isRated}
+                label={{
+                  basic: true,
+                  color: "red",
+                  pointing: "left",
+                  content: (book.rateAmount),
+                }}
+                onClick={() => handleRate(book)}
+              />
             </Segment>
           ))}
         </Segment.Group>
@@ -38,3 +70,4 @@ export default function Books(): ReactElement {
     </div>
   );
 }
+
--- src/services/bookService.ts
+++ src/services/bookService.ts
@@ -6,4 +6,8 @@ export default class BookService {
     async getBooks() {
         return await axios.get("http://localhost:8080/v1/book/getBooks").then(book => book.data) as Book[];
     }
+
+    async updateBook(book: Book) {
+        await axios.put("http://localhost:8080/v1/book/update", book);
+    }
 }
